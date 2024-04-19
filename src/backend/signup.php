1b6e099f944876e58692b871bd3fd705e7e6052b
Anderson <114837609+Anderson2904@users.noreply.github.com>
2024-04-19 11:07:15 -0500

    Add files via upload

---
+++ src/backend/signup.php
@@ -0,0 +1,29 @@
+<?php
+include('../../config/database.php');
+
+    $fullname = $_POST['fname'];
+    $email = $_POST['email'];
+    $passwd = $_POST['passwd'];
+    $enc_pass = md5($passwd);
+
+    /*echo "Your fullname: ". $fullname;
+    echo "Your email: ". $email;
+    echo "Your password: ". $passwd;
+    echo "Your password enc: ". $enc_pass;*/
+
+    $sql = "
+        INSERT INTO users (fullname, email, password) 
+            values('$fullname', '$email', '$enc_pass')
+    ";
+
+    $ans = pg_query($conn,$sql);
+    if ($ans){
+        echo "User has been created succesfully";
+    }else{
+        echo "Error". pg_last_error();
+    }
+
+    //close connection
+    pg_close($conn)
+
+?>
