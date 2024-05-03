08c8405d9495212314ca079774214d6b5f2ff36c
anderson <andersonjavier.31@gmail.com>
2024-05-03 11:52:47 -0500

    envio repositorio

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -11,16 +11,26 @@ include('../../config/database.php');
     echo "Your password: ". $passwd;
     echo "Your password enc: ". $enc_pass;*/
 
-    $sql = "
+    $sql_validate_email = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $sql_validate_email);
+    $total = pg_num_rows($result);
+
+    if($total > 0){
+        echo"<script>alert('Email already exis')</scrpt>";
+        header("refresh:0;url=../signup.html");
+    }else{
+        $sql = "
         INSERT INTO users (fullname, email, password) 
             values('$fullname', '$email', '$enc_pass')
     ";
 
     $ans = pg_query($conn,$sql);
     if ($ans){
+        echo "<script>alert('users has been registers')</script>";
         header("refresh:0;url=../signin.html");
     }else{
         echo "Error". pg_last_error();
+    }
     }
 
     //close connection
